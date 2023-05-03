c76598d9a3a2bfb51e7f7c821ed492e329e3565b
Lukas Scheuerle <lukas.scheuerle@neagen.com>
2023-05-03 09:37:33 +0300

    rename tabs to bootstrap

--- dungeon/bootstrap.ts
+++ dungeon/bootstrap.ts
@@ -1,21 +1,24 @@
+/*
+    Tabs 
+*/
 // find all tabs and give them click detections to switch between contents
 let tabs: NodeListOf<HTMLButtonElement> = document.querySelectorAll("button[role=tab]");
-for(let tab of tabs) {
+for (let tab of tabs) {
     tab.addEventListener("click", activateTab);
-    if(tab.classList.contains("active")) {
-        tab.dispatchEvent(new MouseEvent("click", {relatedTarget: tab}));
+    if (tab.classList.contains("active")) {
+        tab.dispatchEvent(new MouseEvent("click", { relatedTarget: tab }));
     }
 }
 
 function activateTab(event: MouseEvent) {
     let activeTab: HTMLElement = <HTMLElement>event.target;
-    if(!activeTab) return;
-    for(let tab of tabs) {
+    if (!activeTab) return;
+    for (let tab of tabs) {
         tab.classList.remove("active");
         let target: HTMLElement | null = document.querySelector(tab.dataset.tabTarget ?? "");
-        if(!target) continue;
+        if (!target) continue;
         target.classList.add("hidden");
-        if(activeTab.id != tab.id) continue;
+        if (activeTab.id != tab.id) continue;
         target.classList.remove("hidden");
         tab.classList.add("active");
     }
