30a6e55f3dc94fd147ab496925e591cf36386f9f
Lukas Scheuerle <lukas.scheuerle@neagen.com>
2023-05-30 15:20:01 +0300

    update tabs to be reuseable

--- dungeon/bootstrap.ts
+++ dungeon/bootstrap.ts
@@ -1,29 +1,36 @@
 /*
     Tabs 
 */
-// find all tabs and give them click detections to switch between contents
-let tabs: NodeListOf<HTMLButtonElement> = document.querySelectorAll("button[role=tab]");
-for (let tab of tabs) {
-    tab.addEventListener("click", activateTab);
-    if (tab.classList.contains("active")) {
-        tab.dispatchEvent(new MouseEvent("click", { relatedTarget: tab }));
+function setupTabs(){
+    let tabGroups: NodeListOf<HTMLElement> = document.querySelectorAll(".nav-tabs");
+    for(let group of tabGroups){
+        // find all tabs and give them click detections to switch between contents
+        let tabs: NodeListOf<HTMLButtonElement> = group.querySelectorAll("button[role=tab]");
+        for (let tab of tabs) {
+            tab.addEventListener("click", activateTab);
+            if (tab.classList.contains("active")) {
+                tab.dispatchEvent(new MouseEvent("click", { relatedTarget: tab }));
+            }
+        }
+        
+        function activateTab(event: MouseEvent) {
+            let activeTab: HTMLElement = <HTMLElement>event.target;
+            if (!activeTab) return;
+            for (let tab of tabs) {
+                tab.classList.remove("active");
+                let target: HTMLElement | null = document.querySelector(tab.dataset.tabTarget ?? "");
+                if (!target) continue;
+                target.classList.add("hidden");
+                if (activeTab.id != tab.id) continue;
+                target.classList.remove("hidden");
+                tab.classList.add("active");
+            }
+        }
     }
-}
 
-function activateTab(event: MouseEvent) {
-    let activeTab: HTMLElement = <HTMLElement>event.target;
-    if (!activeTab) return;
-    for (let tab of tabs) {
-        tab.classList.remove("active");
-        let target: HTMLElement | null = document.querySelector(tab.dataset.tabTarget ?? "");
-        if (!target) continue;
-        target.classList.add("hidden");
-        if (activeTab.id != tab.id) continue;
-        target.classList.remove("hidden");
-        tab.classList.add("active");
-    }
-}
-
+};
+setupTabs();
+    
 /*
     Notifications
 */
